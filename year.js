8943f6b81db9d1158b8342032a3cb68e9cc5c8f4
yiding26 <yiding26@outlook.com>
2023-05-31 21:17:01 +0800

    second commit

---
+++ year.js
@@ -0,0 +1,10 @@
+function updateCopyrightYear() {
+            var currentYear = new Date().getFullYear();
+            var copyrightElement = document.getElementById("copyright");
+            copyrightElement.textContent = "版权所有 © " + currentYear + " yiding26";
+        }
+        
+// 在页面加载完成后调用函数更新版权年份
+document.addEventListener("DOMContentLoaded", function() {
+    updateCopyrightYear();
+});
