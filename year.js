248aba5d7b9a1fbdac10da5a70e7a423c47374d9
yiding26 <134699160+yiding26@users.noreply.github.com>
2023-06-14 22:12:28 +0800

    Update year.js

--- year.js
+++ year.js
@@ -1,8 +1,8 @@
 function updateCopyrightYear() {
-            var currentYear = new Date().getFullYear();
-            var copyrightElement = document.getElementById("copyright");
-            copyrightElement.textContent = "版权所有 © " + currentYear + " yiding26";
-        }
+    var currentYear = new Date().getFullYear();
+    var copyrightElement = document.getElementById("copyright");
+    copyrightElement.textContent = "版权所有 © " + currentYear + " yiding26 只有专注才能成功！";
+}
         
 // 在页面加载完成后调用函数更新版权年份
 document.addEventListener("DOMContentLoaded", function() {
